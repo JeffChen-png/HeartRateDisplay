36f54c83c352a3193bdb473d54f566c57ef1c44a
JeffChen <medvedrust@mail.ru>
2021-07-01 16:53:05 +0300

    3 section & opt

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+		speed: 500,
+		adaptiveHeight: true,
+  		prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/chevron-left-solid.png" alt=""> </button>',
+  		nextArrow: '<button type="button" class="slick-next"> <img src="../icons/chevron-right-solid.png" alt=""> </button>',
+  		responsive: [
+  		{
+  			breakpoint: 1050,
+  			settings: {
+  				dots: true,
+  				arrows: false, 
+  			}
+  		}]
+	});
+});
