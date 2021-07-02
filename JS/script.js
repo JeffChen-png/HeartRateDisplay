7d2a0622c4ffc193acca1c1c95339c0cb8e940ad
JeffChen <medvedrust@mail.ru>
2021-07-02 13:58:09 +0300

    js fix

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('.carousel__inner').slick({
 		speed: 500,
 		adaptiveHeight: true,
-  		prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/chevron-left-solid.png" alt=""> </button>',
-  		nextArrow: '<button type="button" class="slick-next"> <img src="../icons/chevron-right-solid.png" alt=""> </button>',
+  		prevArrow: '<button type="button" class="slick-prev"> <img src="./icons/chevron-left-solid.png" alt=""> </button>',
+  		nextArrow: '<button type="button" class="slick-next"> <img src="./icons/chevron-right-solid.png" alt=""> </button>',
   		responsive: [
   		{
   			breakpoint: 1050,
