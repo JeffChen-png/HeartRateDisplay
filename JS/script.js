3e1237b345d241863b0001f35f7ebaf0bfda622a
JeffChen <medvedrust@mail.ru>
2021-07-04 23:35:47 +0300

    modal section

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	
+	// Слайдер
+
 	$('.carousel__inner').slick({
 		speed: 500,
 		adaptiveHeight: true,
@@ -14,11 +17,15 @@ $(document).ready(function(){
 		}]
 	});
 
+	//Табы
+
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 	    $(this)
 	    	.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 	    	.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   	});
+
+	//Подробное описание товара
 
   	function toggleSlide(item) {
   		$(item).each(function(i) {
@@ -31,5 +38,28 @@ $(document).ready(function(){
 
   	toggleSlide('div.catalog-item__link');
   	toggleSlide('div.catalog-item__back');
+
+  	//Модальные окна data-modal="consultation || order || thanks"
+
+  	$('[data-modal=consultation]').on('click', function() {
+  		$('.overlay, #consultation').show();
+  	});
+
+  	$('.button_catalog').each(function(i) {
+  		$(this).on('click', function() {
+  			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+  			$('.overlay, #order').show();
+  		});
+  	});
+
+  	$('[data-modal=thanks]').on('click', function() {
+  		$('.overlay, #thanks').show();
+  	});
+
+  	$('.overlay').on('click', function(e) {
+  		if (!$('.modal').is(e.target) && $('.modal').has(e.target).length === 0 || $('.modal__close').is(e.target)) { 
+  			$('.overlay, #consultation, #order, #thanks').hide();
+		}
+  	});
 });
 
